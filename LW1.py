22674e5874f5cbbd3052ae33eb2c49a0dc701255
xdrond <xdrond@gmail.com>
2019-11-18 04:59:12 +0700

    Initial commit

---
+++ LW1.py
@@ -0,0 +1,58 @@
+# coding=utf-8
+import numpy as np
+
+# https://pyprog.pro/io_functions/savetxt.html
+# https://pyprog.pro/array_creation/loadtxt.html
+# https://www.easycoding.org/2017/01/23/schityvaem-chislovye-dannye-iz-fajla-na-python.html
+# Задачи:
+#   1) Решение СЛАУ.
+#   2) Найти определитель матрицы.
+#   3) Поиск обратной матрицы.
+
+fName = 'input.txt'
+
+# Тип задачи.
+taskType = 1
+
+# Размер матрицы.
+matrixSize = 0
+
+# Чтение типа задачи и размера матрицы.
+with open(fName) as inFile:
+    taskType = int(inFile.readline())
+    matrixSize = int(inFile.readline())
+
+# Чтение матрицы коэффициентов из файла
+matrixA = np.loadtxt(fName,
+                     skiprows=2,
+                     usecols=(range(matrixSize)))
+print('Матрица A: ')
+print(matrixA)
+
+
+# Чтение матрицы коэффициентов из файла
+matrixB = np.loadtxt(fName,
+                     skiprows=2,
+                     usecols=matrixSize)
+print('Матрица B: ')
+print(matrixB)
+
+
+# Готовое решение СЛАУ
+x = np.linalg.solve(matrixA, matrixB)
+print('Решение СЛАУ: ')
+print(x)
+
+# Готовое решение определителя матрицы.
+detA = np.linalg.det(matrixA)
+print('Определитель матрицы: ')
+print(detA)
+
+# Готовое решение определителя матрицы.
+invA = np.linalg.inv(matrixA)
+print('Обратная матрица: ')
+print(invA)
+
+
+# Тестовая запись файла.
+np.savetxt('test_output.txt', matrixA, fmt = '%0.2f')
